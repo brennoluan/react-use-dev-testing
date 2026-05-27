bc50470c610aa3b69923d3d7db501a6bed6ea8ed
Brenno Luan <brennoluan8530@gmail.com>
2026-05-27 16:52:04 -0300

    Enhance Jest configuration and TypeScript settings for testing

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      { tsconfig: "<rootDir>/tsconfig.test.json" },
+    ],
   },
+
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
